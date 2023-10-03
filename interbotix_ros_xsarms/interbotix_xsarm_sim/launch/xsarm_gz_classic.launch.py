6c3b89f2c028b9db0caf006184f28bc8cf1f8e06
decarabas <codrea.eric@gmail.com>
2023-10-03 17:30:01 -0500

    Added line which looks inside of artefacts_demo_control for cube model

--- interbotix_ros_xsarms/interbotix_xsarm_sim/launch/xsarm_gz_classic.launch.py
+++ interbotix_ros_xsarms/interbotix_xsarm_sim/launch/xsarm_gz_classic.launch.py
@@ -69,10 +69,6 @@ def launch_setup(context, *args, **kwargs):
     robot_description_launch_arg = LaunchConfiguration('robot_description')
     hardware_type_launch_arg = LaunchConfiguration('hardware_type')
 
-    ###########################
-    motor_configs_launch_arg = LaunchConfiguration('motor_configs')
-    mode_configs_launch_arg = LaunchConfiguration('mode_configs')
-    ##########################
 
     # sets use_sim_time parameter to 'true' if using gazebo hardware
     use_sim_time_param = determine_use_sim_time_param(
@@ -92,8 +88,16 @@ def launch_setup(context, *args, **kwargs):
             ).parent.resolve()),
             ':',
             str(Path(
+                FindPackageShare('artefacts_demo_control').perform(context) # Added path to artefacts models
+            ).parent.resolve()),
+            ':',
+            str(Path(
                 FindPackageShare('interbotix_xsarm_descriptions').perform(context)
             ).parent.resolve()),
+            # ':',
+            # str(Path(
+            #     FindPackageShare('artefacts_demo_control').perform(context) # Added path to artefacts models
+            # ).parent.resolve()),
         ]
     )
 
@@ -188,20 +192,6 @@ def launch_setup(context, *args, **kwargs):
         }]
     )
 
-    xs_sdk_sim_node = Node(
-        package='interbotix_xs_sdk',
-        executable='xs_sdk_sim.py',
-        name='xs_sdk_sim',
-        namespace=robot_name_launch_arg,
-        arguments=[],
-        parameters=[{
-            'motor_configs': motor_configs_launch_arg,
-            'mode_configs': mode_configs_launch_arg,
-            'robot_description': robot_description_launch_arg,
-            'use_sim_time': use_sim_time_param,
-        }],
-        output={'both': 'screen'},
-    )
     xsarm_description_launch_include = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
@@ -250,7 +240,7 @@ def launch_setup(context, *args, **kwargs):
         load_arm_controller_event,
         load_gripper_controller_event,
         xsarm_description_launch_include,
-        xs_sdk_sim_node
+        # xs_sdk_sim_node
     ]
 
 
